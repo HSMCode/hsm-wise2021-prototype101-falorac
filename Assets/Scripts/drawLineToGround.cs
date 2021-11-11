d203fb230024d01a58e75650ceef3db1b7ae3118
falorac <carola.feierfeil@students.hs-mainz.de>
2021-11-11 14:08:29 +0100

    Finished debugAssignment created prototype101 scene, added ground, four game objects and attached materials, added three different debug scripts (ground clipping, object name and position, drawing line to ground)

---
+++ Assets/Scripts/drawLineToGround.cs
@@ -0,0 +1,23 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class drawLineToGround : MonoBehaviour
+{
+    // Start is called before the first frame update
+    void Start()
+    {
+        /*
+        *   The GameObject draws a blue line to the created Ground plane 
+        *   for five seconds in scene view.
+        */
+        float height = transform.position.y;
+        Debug.DrawLine(transform.position, transform.position - Vector3.up * height, Color.blue, 5);
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+}
---
+++ Assets/Scripts/giveNameAndPosition.cs
@@ -0,0 +1,21 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class giveNameAndPosition : MonoBehaviour
+{
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        // GameObject the script is attached to says its "name" aka the name you gave it within the hierarchy
+        Debug.Log("Hello, my name is " + this.gameObject + "!", this.gameObject);
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        // GameObject the script is attached to states x, y, z coordinates every frame
+        Debug.Log(this.gameObject + " is positioned at " + this.gameObject.transform.position, this.gameObject);
+    }
+}
